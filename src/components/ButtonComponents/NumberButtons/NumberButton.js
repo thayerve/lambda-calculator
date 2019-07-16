4b0c35f85ddfbdcf9fade1540c38a38bb5d7d770
Virginia Thayer <virginia@stripe.com>
2019-07-16 12:35:00 -0700

    initial commit. exported data modules.

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const NumberButton = () => {
   return (
-    <>
+    <button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    </button>
   );
 };
